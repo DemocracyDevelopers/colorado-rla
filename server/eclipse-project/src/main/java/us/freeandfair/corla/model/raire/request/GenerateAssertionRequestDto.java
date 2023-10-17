7fa67baeb75d1c6f6c65d66e403423e0b8430d1a
Amit Mahindrakar <amit@democracydevelopers.org.au>
2023-10-17 12:12:43 +1100

    Added raire connector code

---
+++ server/eclipse-project/src/main/java/us/freeandfair/corla/model/raire/request/GenerateAssertionRequestDto.java
@@ -0,0 +1,16 @@
+package us.freeandfair.corla.model.raire.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class GenerateAssertionRequestDto {
+  private Long contestId;
+  private Integer totalAuditableBallots;
+  private Integer timeProvisionForResult;
+}
---
+++ server/eclipse-project/src/main/java/us/freeandfair/corla/model/raire/response/RaireResponse.java
@@ -0,0 +1,19 @@
+package us.freeandfair.corla.model.raire.response;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RaireResponse {
+  private JsonNode metadata;
+  public Map<String, AssertionPermutations> solution;
+
+
+
+
+}
