5f4a6d36433b71d6cdc93f32d257c60c7040e0b9
michelleblom <michelle.blom@gmail.com>
2023-10-17 22:51:09 +1100

    Added simple tests for creation and persistence of NEB and NEN assertions

---
+++ server/eclipse-project/src/test/java/us/freeandfair/corla/model/AssertionTest.java
@@ -0,0 +1,70 @@
+package us.freeandfair.corla.model;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import us.freeandfair.corla.persistence.Persistence;
+import us.freeandfair.corla.query.AssertionQueries;
+import us.freeandfair.corla.query.Setup;
+
+
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class AssertionTest {
+
+  private AssertionTest() {};
+
+
+  @BeforeTest()
+  public void setUp() {
+    Setup.setProperties();
+    Persistence.beginTransaction();
+
+  }
+
+  @AfterTest()
+  public void tearDown() {
+    try {
+      Persistence.rollbackTransaction();
+    } catch (Exception e) {
+    }
+  }
+
+  @Test()
+  public void testCreateNEB() {
+    NEBAssertion neb = new NEBAssertion("Board of Tax and Estimation", "Alice", "Bob",
+            50, 5000, 100);
+
+    Persistence.save(neb);
+    Persistence.flushAndClear();
+
+    List<Assertion> assertions = AssertionQueries.matching("Board of Tax and Estimation");
+
+    Assertion neb_act = assertions.get(0);
+    assertEquals("Alice", neb_act.getWinner());
+    assertEquals("Bob", neb_act.getLoser());
+    assertEquals("Board of Tax and Estimation", neb_act.getContestName());
+  }
+
+  @Test()
+  public void testCreateNEN() {
+    NENAssertion nen = new NENAssertion("Board of Play", "Chuan", "Alice",
+            30, 4000, 133.3, Arrays.asList("Chuan", "Alice", "Diego"));
+
+    Persistence.save(nen);
+    Persistence.flushAndClear();
+
+    List<Assertion> assertions = AssertionQueries.matching("Board of Play");
+
+    Assertion nen_act = assertions.get(0);
+    assertEquals("Chuan", nen_act.getWinner());
+    assertEquals("Alice", nen_act.getLoser());
+    assertEquals("Board of Play", nen_act.getContestName());
+    assertEquals(Arrays.asList("Chuan", "Alice", "Diego"), nen_act.getContinuing());
+  }
+
+}
