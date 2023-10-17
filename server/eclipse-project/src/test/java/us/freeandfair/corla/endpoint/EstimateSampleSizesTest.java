eef4e82151c72559305c16bcc5aa23d27dbbe95c
michelleblom <michelle.blom@gmail.com>
2023-10-17 11:27:12 +1100

    Progress on getting estimate sample sizes test working; Need to add more data to database (eg. ballot manifests).

--- server/eclipse-project/src/test/java/us/freeandfair/corla/endpoint/EstimateSampleSizesTest.java
+++ server/eclipse-project/src/test/java/us/freeandfair/corla/endpoint/EstimateSampleSizesTest.java
@@ -1,17 +1,20 @@
 package us.freeandfair.corla.endpoint;
 
-import org.hibernate.Session;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import us.freeandfair.corla.model.*;
 import us.freeandfair.corla.persistence.Persistence;
+import us.freeandfair.corla.query.CountyQueries;
 import us.freeandfair.corla.query.Setup;
 
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class EstimateSampleSizesTest {
@@ -25,10 +28,11 @@ public class EstimateSampleSizesTest {
     Persistence.beginTransaction();
 
     // Create DoSDashboard with some audit information.
-    DoSDashboard dosb = new DoSDashboard();
-    dosb.updateAuditInfo(new AuditInfo("general", Instant.now(), Instant.now(),
+    DoSDashboard dosdb = Persistence.getByID(DoSDashboard.ID, DoSDashboard.class);
+    dosdb.updateAuditInfo(new AuditInfo("general", Instant.now(), Instant.now(),
             "12856782643571354365", BigDecimal.valueOf(5)));
-    Persistence.saveOrUpdate(dosb);
+    Persistence.saveOrUpdate(dosdb);
+    Persistence.flush();
   }
 
   @AfterTest()
@@ -39,39 +43,6 @@ public class EstimateSampleSizesTest {
     }
   }
 
-  @Test
-  public void testEstimateSampleSizePlurality(){
-
-    // Add data for county contest results to the database
-    final Session s = Persistence.currentSession();
-
-    /*String query = "insert into contest (id,description,name,sequence_number,votes_allowed,winners_allowed,county_id) values "
-            + " (0,'PLURALITY','Board of Parks',0,1,1,48), (1,'PLURALITY','Board of Tax and Estimation',1,1,1,49)";
-    s.createNativeQuery(query).executeUpdate();
-
-    query = "insert into contest_choice (contest_id,fictitious,name,qualified_write_in,index) values "
-            + " (0,false,'Alice',false,0), (0,false,'Bob',false,1), (0,false,'Chuan',false,2), (0,false,'Diego',false,3), "
-            + " (1,false,'Alice',false,0), (1,false,'Bob',false,1), (1,false,'Chuan',false,2), (1,false,'Diego',false,3) ";
-    s.createNativeQuery(query).executeUpdate();
-
-    query = " insert into county_contest_result "
-            + "(id,contest_ballot_count,county_ballot_count,losers,max_margin,min_margin,winners,winners_allowed,contest_id,county_id) "
-            + "values (0,170,170,'Alice,Bob,Diego',90,10,'Chuan',1,0,48), (1,225,225,'Alice,Bob,Chuan',145,15,'Diego',1,1,49)";
-    s.createNativeQuery(query).executeUpdate();
-
-    query = "insert into county_contest_vote_total (result_id,vote_total,choice) values "
-            + " (0,20,'Alice'), (0,10,'Bob'), (0,100,'Chuan'), (0,40,'Diego'), "
-            + " (1,50,'Alice'), (1,5,'Bob'), (1,20,'Chuan'), (1,150,'Diego') ";
-    s.createNativeQuery(query).executeUpdate();*/
-
-    EstimateSampleSizes esr = new EstimateSampleSizes();
-    Map<String,Integer> samples = esr.estimateSampleSizes();
-
-    // TODO: compare sample sizes to known estimates.
-    System.out.println(samples);
-  }
-
-  /*
   @Test()
   public void testEstimateSampleSizesSimplePlurality() {
     // For testing sample size estimation endpoint, we need a series of CountyContestResult's
@@ -79,9 +50,10 @@ public class EstimateSampleSizesTest {
 
     County cty = CountyQueries.fromString("Boulder");
 
-    Persistence.saveOrUpdate(cty);
+    // To do: need to add ballot manifest data.
 
     List<String> candidates = Arrays.asList("Alice", "Bob", "Chuan", "Diego");
+
     List<Choice> choices = candidates.stream().map(c -> { return new Choice(c,
             "", false, false);}).collect(Collectors.toList());
 
@@ -113,11 +85,16 @@ public class EstimateSampleSizesTest {
     ctr.updateResults();
 
     Persistence.saveOrUpdate(ctr);
+    Persistence.flush();
 
     List<CountyContestResult> contestResults = Persistence.getAll(CountyContestResult.class);
 
-    System.out.println(contestResults.size());
+    assertEquals(contestResults.size(), 1);
 
+    EstimateSampleSizes esr = new EstimateSampleSizes();
+    Map<String,Integer> samples = esr.estimateSampleSizes();
+
+    System.out.println(samples);
   }
 
   private CastVoteRecord createVoteFor(final String name, final Contest co, final County cty, Integer position){
@@ -142,6 +119,6 @@ public class EstimateSampleSizesTest {
             contest_info);
     Persistence.save(cvr);
     return cvr;
-  }*/
+  }
 
 }
