65388267b70c42d986bab37a9eb0ae64234d80d2
Vanessa Teague <vanessafromgit@thinkingcybersecurity.com>
2024-05-26 20:45:29 +1000

    Stub files for models.

---
+++ server/eclipse-project/src/main/java/au/org/democracydevelopers/corla/raire/requestToRaire/GetAssertionsRequest.java
@@ -0,0 +1,25 @@
+/*
+Democracy Developers IRV extensions to colorado-rla.
+
+@copyright 2024 Colorado Department of State
+
+These IRV extensions are designed to connect to a running instance of the raire 
+service (https://github.com/DemocracyDevelopers/raire-service), in order to 
+generate assertions that can be audited using colorado-rla.
+
+The colorado-rla IRV extensions are free software: you can redistribute it and/or modify it under the terms
+of the GNU Affero General Public License as published by the Free Software Foundation, either
+version 3 of the License, or (at your option) any later version.
+
+The colorado-rla IRV extensions are distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along with
+raire-service. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+package au.org.democracydevelopers.corla.raire.requestToRaire;
+
+public class GetAssertionsRequest {
+}
