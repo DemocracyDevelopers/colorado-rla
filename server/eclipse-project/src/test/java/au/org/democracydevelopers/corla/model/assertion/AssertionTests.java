a4650b91e1be9f30919e3240938e223538d50a2e
michelleblom <michelleblom@users.noreply.github.com>
2024-06-08 12:52:09 +1000

    Switch to using parametrised tests to verify optimistic sample size computation for assertions.

--- server/eclipse-project/src/test/java/au/org/democracydevelopers/corla/model/assertion/AssertionTests.java
+++ server/eclipse-project/src/test/java/au/org/democracydevelopers/corla/model/assertion/AssertionTests.java
@@ -21,13 +21,245 @@ raire-service. If not, see <https://www.gnu.org/licenses/>.
 
 package au.org.democracydevelopers.corla.model.assertion;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-import org.testng.annotations.Test;
+import static java.lang.Math.ceil;
+import static java.lang.Math.log;
+import static java.lang.Math.max;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+import org.testng.annotations.DataProvider;
+
+/**
+ * This class does not contain any tests, but provides utilities for use by the
+ * NEB/NEN Assertion test classes. Although much of the assertion-related functionality is
+ * present in the abstract Assertion class, we have chosen to test this functionality
+ * twice -- once in the context where the assertion is an NEB and then in the context
+ * where the assertion is an NEN. Although, at present, it is the same code that is
+ * being tested in both cases, it is possible that future development may alter the
+ * assertion subclasses in potentially unforeseen ways. This extra level of testing will
+ * reduce the likelihood of future programming errors if this eventuates.
+ */
 public class AssertionTests {
 
-  private static final Logger LOGGER = LogManager.getLogger(AssertionTests.class);
+  /**
+   * A 10% risk limit, as a BigDecimal.
+   */
+  public static final BigDecimal riskLimit10 = BigDecimal.valueOf(0.10);
 
+  /**
+   * A small raw margin of 10 votes.
+   */
+  public static final Integer smallRawMargin = 10;
 
+  /**
+   * A small diluted margin of 0.001.
+   */
+  public static final BigDecimal smallMargin = BigDecimal.valueOf(0.001);
+
+  /**
+   * A high assertion difficulty value of 100.
+   */
+  public static final BigDecimal highDifficulty = BigDecimal.valueOf(100);
+
+  /**
+   * A large raw margin of 10000 votes.
+   */
+  public static final Integer largeRawMargin = 10000;
+
+  /**
+   * A large diluted margin of 0.55.
+   */
+  public static final BigDecimal largeMargin = BigDecimal.valueOf(0.55);
+
+  /**
+   * A small assertion difficulty of 1.
+   */
+  public static final BigDecimal smallDifficulty = BigDecimal.valueOf(1);
+
+  /**
+   * Returns a set of varying parameters to supply when constructing assertions to test
+   * sample size computations. To test sample size calculation, we consider the following dimensions:
+   * -- Assertions with large/small diluted margins.
+   * -- Assertions with varying combinations of discrepancies:
+   *    -- No discrepancies;
+   *    -- 1 of each type of discrepancy, rest 0.
+   *    -- 2 of each type of discrepancy, rest 0.
+   *    -- Combinations of discrepancies including overstatements. We could have lots of
+   *       combinations here, but given that overstatements are of the highest concern, we
+   *       consider 1 one vote overstatement plus 1 of each other type (alternating) and
+   *       1 two vote overstatement plus 1 of each other type (alternating).
+   *    -- Combinations of discrepancies including no overstatements (1 of each type).
+   * @return An array of parameter arrays, each containing one combination of assertion attributes
+   * that we want to test sample size computation on.
+   */
+  @DataProvider(name = "SampleParameters")
+  public static Object[][] sampleParameters(){
+    // Parameter list: raw margin, diluted margin, difficulty, cvr discrepancy map,
+    // one vote overs, one vote unders, two vote overs, two vote unders, others.
+    return new Object[][]{
+        // Small margin cases
+        // No discrepancies
+        {smallRawMargin, smallMargin, highDifficulty, Map.of(), 0, 0, 0, 0, 0},
+        // one 1 vote understatement
+        {smallRawMargin, smallMargin, highDifficulty, Map.of(1L, -1), 0, 1, 0, 0, 0},
+        // one 1 vote overstatement
+        {smallRawMargin, smallMargin, highDifficulty, Map.of(1L, 1), 1, 0, 0, 0, 0},
+        // one 2 vote understatement
+        {smallRawMargin, smallMargin, highDifficulty, Map.of(1L, -2), 0, 0, 0, 1, 0},
+        // one 2 vote overstatement
+        {smallRawMargin, smallMargin, highDifficulty, Map.of(1L, 2), 0, 0, 1, 0, 0},
+        // one other discrepancy
+        {smallRawMargin, smallMargin, highDifficulty, Map.of(1L, 0), 0, 0, 0, 0, 1},
+
+        // two 1 vote understatements
+        {smallRawMargin, smallMargin, highDifficulty, Map.of(1L, -1, 2L, -1), 0, 2, 0, 0, 0},
+        // two 1 vote overstatements
+        {smallRawMargin, smallMargin, highDifficulty, Map.of(1L, 1, 2L, 1), 2, 0, 0, 0, 0},
+        // two 2 vote understatements
+        {smallRawMargin, smallMargin, highDifficulty, Map.of(1L, -2, 2L, -2), 0, 0, 0, 2, 0},
+        // two 2 vote overstatements
+        {smallRawMargin, smallMargin, highDifficulty, Map.of(1L, 2, 2L, 2), 0, 0, 2, 0, 0},
+        // two other discrepancies
+        {smallRawMargin, smallMargin, highDifficulty, Map.of(1L, 0, 2L, 0), 0, 0, 0, 0, 2},
+
+        // one 1 vote overstatement, one 1 vote understatement
+        {smallRawMargin, smallMargin, highDifficulty, Map.of(1L, 1, 2L, -1), 1, 1, 0, 0, 0},
+        // one 1 vote overstatement, one 2 vote overstatement
+        {smallRawMargin, smallMargin, highDifficulty, Map.of(1L, 1, 2L, 2), 1, 0, 1, 0, 0},
+        // one 1 vote overstatement, one 2 vote understatement
+        {smallRawMargin, smallMargin, highDifficulty, Map.of(1L, 1, 2L, -2), 1, 0, 0, 1, 0},
+        // one 1 vote overstatement, one other discrepancy
+        {smallRawMargin, smallMargin, highDifficulty, Map.of(1L, 1, 2L, 0), 1, 0, 0, 0, 1},
+
+        // one 2 vote overstatement, one 1 vote understatement
+        {smallRawMargin, smallMargin, highDifficulty, Map.of(1L, 2, 2L, -1), 0, 1, 1, 0, 0},
+        // one 2 vote overstatement, one 2 vote understatement
+        {smallRawMargin, smallMargin, highDifficulty, Map.of(1L, 2, 2L, -2), 0, 0, 1, 1, 0},
+        // one 2 vote overstatement, one other discrepancy
+        {smallRawMargin, smallMargin, highDifficulty, Map.of(1L, 2, 2L, 0), 0, 0, 1, 0, 1},
+
+        // one discrepancy of each type
+        {smallRawMargin, smallMargin, highDifficulty, Map.of(1L, 1, 2L, -1, 3L,
+            2, 4L, -2, 5L, 0), 1, 1, 1, 1, 1},
+
+        // one discrepancy of each type (excluding overstatements)
+        {smallRawMargin, smallMargin, highDifficulty, Map.of(2L, -1, 4L, -2,
+            5L, 0), 0, 1, 0, 1, 1},
+
+        // Large margin cases
+        // No discrepancies
+        {largeRawMargin, largeMargin, smallDifficulty, Map.of(), 0, 0, 0, 0, 0},
+        // one 1 vote understatement
+        {largeRawMargin, largeMargin, smallDifficulty, Map.of(1L, -1), 0, 1, 0, 0, 0},
+        // one 1 vote overstatement
+        {largeRawMargin, largeMargin, smallDifficulty, Map.of(1L, 1), 1, 0, 0, 0, 0},
+        // one 2 vote understatement
+        {largeRawMargin, largeMargin, smallDifficulty, Map.of(1L, -2), 0, 0, 0, 1, 0},
+        // one 2 vote overstatement
+        {largeRawMargin, largeMargin, smallDifficulty, Map.of(1L, 2), 0, 0, 1, 0, 0},
+        // one other discrepancy
+        {largeRawMargin, largeMargin, smallDifficulty, Map.of(1L, 0), 0, 0, 0, 0, 1},
+
+        // two 1 vote understatements
+        {largeRawMargin, largeMargin, smallDifficulty, Map.of(1L, -1, 2L, -1), 0, 2, 0, 0, 0},
+        // two 1 vote overstatements
+        {largeRawMargin, largeMargin, smallDifficulty, Map.of(1L, 1, 2L, 1), 2, 0, 0, 0, 0},
+        // two 2 vote understatements
+        {largeRawMargin, largeMargin, smallDifficulty, Map.of(1L, -2, 2L, -2), 0, 0, 0, 2, 0},
+        // two 2 vote overstatements
+        {largeRawMargin, largeMargin, smallDifficulty, Map.of(1L, 2, 2L, 2), 0, 0, 2, 0, 0},
+        // two other discrepancies
+        {largeRawMargin, largeMargin, smallDifficulty, Map.of(1L, 0, 2L, 0), 0, 0, 0, 0, 2},
+
+        // one 1 vote overstatement, one 1 vote understatement
+        {largeRawMargin, largeMargin, smallDifficulty, Map.of(1L, 1, 2L, -1), 1, 1, 0, 0, 0},
+        // one 1 vote overstatement, one 2 vote overstatement
+        {largeRawMargin, largeMargin, smallDifficulty, Map.of(1L, 1, 2L, 2), 1, 0, 1, 0, 0},
+        // one 1 vote overstatement, one 2 vote understatement
+        {largeRawMargin, largeMargin, smallDifficulty, Map.of(1L, 1, 2L, -2), 1, 0, 0, 1, 0},
+        // one 1 vote overstatement, one other discrepancy
+        {largeRawMargin, largeMargin, smallDifficulty, Map.of(1L, 1, 2L, 0), 1, 0, 0, 0, 1},
+
+        // one 2 vote overstatement, one 1 vote understatement
+        {largeRawMargin, largeMargin, smallDifficulty, Map.of(1L, 2, 2L, -1), 0, 1, 1, 0, 0},
+        // one 2 vote overstatement, one 2 vote understatement
+        {largeRawMargin, largeMargin, smallDifficulty, Map.of(1L, 2, 2L, -2), 0, 0, 1, 1, 0},
+        // one 2 vote overstatement, one other discrepancy
+        {largeRawMargin, largeMargin, smallDifficulty, Map.of(1L, 2, 2L, 0), 0, 0, 1, 0, 1},
+
+        // one discrepancy of each type
+        {largeRawMargin, largeMargin, smallDifficulty, Map.of(1L, 1, 2L, -1, 3L,
+            2, 4L, -2, 5L, 0), 1, 1, 1, 1, 1},
+
+        // one discrepancy of each type (excluding overstatements)
+        {largeRawMargin, largeMargin, smallDifficulty, Map.of(2L, -1, 4L, -2,
+            5L, 0), 0, 1, 0, 1, 1}
+    };
+  }
+
+  /**
+   * Populate the attributes of the given assertion according to the given parameters.
+   * @param a Assertion to be configured.
+   * @param winner Winner of the assertion.
+   * @param loser Loser of the assertion.
+   * @param contestName Name of the contest to which the assertion belongs.
+   * @param rawMargin Raw margin of the assertion.
+   * @param dilutedMargin Diluted margin of the assertion.
+   * @param difficulty Difficulty of the assertion.
+   * @param cvrDiscrepancy Map between CVR ID and discrepancy type.
+   * @param oneVoteOver Number of one vote overstatements to associate with the assertion.
+   * @param oneVoteUnder Number of one vote understatements to associate with the assertion.
+   * @param twoVoteOver Number of two vote overstatements to associate with the assertion.
+   * @param twoVoteUnder Number of two vote understatements to associate with the assertion.
+   * @param other Number of other discrepancies to associate with the assertion.
+   */
+  public static void populateAssertion(Assertion a, String winner, String loser, String contestName,
+      List<String> continuing, int rawMargin, double dilutedMargin, double difficulty,
+      Map<Long,Integer> cvrDiscrepancy, int oneVoteOver, int oneVoteUnder, int twoVoteOver,
+      int twoVoteUnder, int other){
+
+    a.winner = winner;
+    a.loser  = loser;
+    a.contestName = contestName;
+    a.assumedContinuing = continuing;
+    a.margin = rawMargin;
+    a.dilutedMargin = BigDecimal.valueOf(dilutedMargin);
+    a.difficulty = difficulty;
+    a.cvrDiscrepancy = cvrDiscrepancy;
+    a.oneVoteOverCount = oneVoteOver;
+    a.oneVoteUnderCount = oneVoteUnder;
+    a.twoVoteOverCount = twoVoteOver;
+    a.twoVoteUnderCount = twoVoteUnder;
+    a.otherCount = other;
+  }
+
+  /**
+   * Compute the optimistic number of ballots to sample for an assertion with the given
+   * parameters, and the given risk limit. This function implements the sample size
+   * estimation as described in Section 2.2.1 of the Colorado IRV RLA Implementation Plan.
+   * @param riskLimit Risk limit of the audit.
+   * @param dilutedMargin Diluted margin of the assertion.
+   * @param o1 Number of one vote overstatements.
+   * @param o2 Number of two vote overstatements.
+   * @param u1 Number of one vote understatements.
+   * @param u2 Number of two vote understatements.
+   * @param gamma Gamma parameter.
+   * @return the optimistic number of ballots to sample when auditing the assertion with
+   * the given characteristics.
+   */
+  public static int optimistic(BigDecimal riskLimit, double dilutedMargin, int o1, int o2,
+      int u1, int u2, BigDecimal gamma){
+
+    final double dgamma = gamma.doubleValue();
+    final double oneOnOneGamma = 1/dgamma;
+    final double oneOnTwoGamma = 1/(2*dgamma);
+    final double factor = (-2 * dgamma)/dilutedMargin;
+
+    final int totalDiscrepancies = o1 + o2 + u1 + u2;
+
+    return max(totalDiscrepancies, (int)ceil(factor * (log(riskLimit.doubleValue()) +
+        u1 * log(1 + oneOnTwoGamma) + u2 * log(1 + oneOnOneGamma) + o1 * log(1 - oneOnTwoGamma) +
+        o2 * log(1 - oneOnOneGamma))));
+  }
 }
--- server/eclipse-project/src/test/java/au/org/democracydevelopers/corla/model/assertion/NEBAssertionTests.java
+++ server/eclipse-project/src/test/java/au/org/democracydevelopers/corla/model/assertion/NEBAssertionTests.java
@@ -21,10 +21,6 @@ raire-service. If not, see <https://www.gnu.org/licenses/>.
 
 package au.org.democracydevelopers.corla.model.assertion;
 
-import static java.lang.Math.ceil;
-import static java.lang.Math.exp;
-import static java.lang.Math.log;
-import static java.lang.Math.max;
 import static org.testng.Assert.assertEquals;
 
 import java.math.BigDecimal;
@@ -35,11 +31,13 @@ import org.apache.log4j.Logger;
 import org.testng.annotations.Test;
 import us.freeandfair.corla.math.Audit;
 
+/**
+ * A suite of tests to verify the functionality of NEBAssertion objects. This includes:
+ * -- Testing of optimistic sample size.
+ */
 public class NEBAssertionTests {
 
   private static final Logger LOGGER = LogManager.getLogger(NEBAssertionTests.class);
-
-  private final BigDecimal riskLimit10 = BigDecimal.valueOf(0.10);
 
   /**
    * Create an NEB assertion with the given parameters.
@@ -58,324 +56,45 @@ public class NEBAssertionTests {
    * @return an NEB assertion with the given specification.
    */
   public static Assertion createNEBAssertion(String winner, String loser, String contestName,
-      int rawMargin, double dilutedMargin, double difficulty,
-      Map<Long,Integer> cvrDiscrepancy, int oneVoteOver, int oneVoteUnder,
-      int twoVoteOver, int twoVoteUnder, int other){
+      int rawMargin, double dilutedMargin, double difficulty, Map<Long,Integer> cvrDiscrepancy,
+      int oneVoteOver, int oneVoteUnder, int twoVoteOver, int twoVoteUnder, int other){
 
     Assertion a = new NEBAssertion();
-    a.winner = winner;
-    a.loser  = loser;
-    a.contestName = contestName;
-    a.margin = rawMargin;
-    a.dilutedMargin = BigDecimal.valueOf(dilutedMargin);
-    a.difficulty = difficulty;
-    a.cvrDiscrepancy = cvrDiscrepancy;
-    a.oneVoteOverCount = oneVoteOver;
-    a.oneVoteUnderCount = oneVoteUnder;
-    a.twoVoteOverCount = twoVoteOver;
-    a.twoVoteUnderCount = twoVoteUnder;
-    a.otherCount = other;
+    AssertionTests.populateAssertion(a, winner, loser, contestName, List.of(), rawMargin,
+    dilutedMargin, difficulty, cvrDiscrepancy, oneVoteOver, oneVoteUnder, twoVoteOver,
+    twoVoteUnder, other);
 
     return a;
   }
 
+
   /**
-   * Compute the optimistic number of ballots to sample for an assertion with the given
-   * parameters, and the given risk limit. This function implements the sample size
-   * estimation as described in Section 2.2.1 of the Colorado IRV RLA Implementation Plan.
-   * @param riskLimit Risk limit of the audit.
+   * This suite of tests verifies the optimistic sample size computation for NEB assertions.
+   * @param rawMargin Raw margin of the assertion.
    * @param dilutedMargin Diluted margin of the assertion.
-   * @param o1 Number of one vote overstatements.
-   * @param o2 Number of two vote overstatements.
-   * @param u1 Number of one vote understatements.
-   * @param u2 Number of two vote understatements.
-   * @param gamma Gamma parameter.
-   * @return the optimistic number of ballots to sample when auditing the assertion with
-   * the given characteristics.
+   * @param difficulty Raire-computed difficulty of the assertion.
+   * @param cvrDiscrepancies Map between CVR id and associated discrepancy for the assertion.
+   * @param oneVoteOver Number of one vote overstatements related to the assertion.
+   * @param oneVoteUnder Number of one vote understatements related to the assertion.
+   * @param twoVoteOver Number of two vote overstatements related to the assertion.
+   * @param twoVoteUnder Number of two vote understatements related to the assertion.
+   * @param other Number of other discrepancies related to the assertion.
    */
-  public static int optimistic(BigDecimal riskLimit, double dilutedMargin, int o1, int o2,
-      int u1, int u2, BigDecimal gamma){
-    final double dgamma = gamma.doubleValue();
-    final double oneOnOneGamma = 1/dgamma;
-    final double oneOnTwoGamma = 1/(2*dgamma);
-    final double factor = (-2 * dgamma)/dilutedMargin;
+  @Test(dataProvider = "SampleParameters", dataProviderClass = AssertionTests.class)
+  public void testNEBOptimistic(Integer rawMargin, BigDecimal dilutedMargin, BigDecimal difficulty,
+      Map<Long,Integer> cvrDiscrepancies, Integer oneVoteOver, Integer oneVoteUnder,
+      Integer twoVoteOver, Integer twoVoteUnder, Integer other){
 
-    final int totalDiscrepancies = o1 + o2 + u1 + u2;
-
-    return max(totalDiscrepancies, (int)ceil(factor * (log(riskLimit.doubleValue()) +
-        u1 * log(1 + oneOnTwoGamma) + u2 * log(1 + oneOnOneGamma) + o1 * log(1 - oneOnTwoGamma) +
-        o2 * log(1 - oneOnOneGamma))));
-  }
-
-  // To test sample size calculation, we consider the following dimensions:
-  // -- Assertions with large/small diluted margins.
-  // -- Assertions with varying combinations of discrepancies:
-  //      -- No discrepancies;
-  //      -- 1 of each type of discrepancy, rest 0.
-  //      -- 2 of each type of discrepancy, rest 0.
-  //      -- Combinations of discrepancies including overstatements. We could have lots of
-  //         combinations here, but given that overstatements are of the highest concern, we
-  //         consider 1 one vote overstatement plus 1 of each other type (alternating) and
-  //         1 two vote overstatement plus 1 of each other type (alternating).
-  //      -- Combinations of discrepancies including no overstatements (1 of each type).
-
-  // Dimension: small diluted margin
-
-  /**
-   * Check that the correct optimistic sample size is computed for an NEB assertion
-   * with a small diluted margin and no discrepancies.
-   */
-  @Test
-  public void testNEBOptimisticSampleNoDiscrepanciesSmallMargin(){
     Assertion a = createNEBAssertion("W", "L", "Test Contest",
-        10, 0.001, 100, Map.of(), 0,
-        0, 0, 0, 0);
+        rawMargin, dilutedMargin.doubleValue(), difficulty.doubleValue(), cvrDiscrepancies,
+        oneVoteOver, oneVoteUnder, twoVoteOver, twoVoteUnder, other);
 
-    final int result = a.computeOptimisticSamplesToAudit(riskLimit10);
-    final int expected = optimistic(riskLimit10, 0.001, 0, 0, 0, 0,
-        Audit.GAMMA);
+    final int result = a.computeOptimisticSamplesToAudit(AssertionTests.riskLimit10);
+    final int expected = AssertionTests.optimistic(AssertionTests.riskLimit10,
+        dilutedMargin.doubleValue(), oneVoteOver, twoVoteOver, oneVoteUnder, twoVoteUnder, Audit.GAMMA);
 
     assertEquals(result, expected);
     assertEquals(a.optimisticSamplesToAudit.intValue(), expected);
   }
 
-  /**
-   * Check that the correct optimistic sample size is computed for an NEB assertion
-   * with a small diluted margin and one 1-vote understatement.
-   */
-  @Test
-  public void testNEBOptimisticSampleOneOneVoteUnderSmallMargin(){
-    Assertion a = createNEBAssertion("W", "L", "Test Contest",
-        10, 0.001, 100, Map.of(1L, -1), 0,
-        1, 0, 0, 0);
-
-    final int result = a.computeOptimisticSamplesToAudit(riskLimit10);
-    final int expected = optimistic(riskLimit10, 0.001, 0, 0, 1, 0,
-        Audit.GAMMA);
-
-    assertEquals(result, expected);
-    assertEquals(a.optimisticSamplesToAudit.intValue(), expected);
-  }
-
-  /**
-   * Check that the correct optimistic sample size is computed for an NEB assertion
-   * with a small diluted margin and one 1-vote overstatement.
-   */
-  @Test
-  public void testNEBOptimisticSampleOneOneVoteOverSmallMargin(){
-    Assertion a = createNEBAssertion("W", "L", "Test Contest",
-        10, 0.001, 100, Map.of(1L, 1), 1,
-        0, 0, 0, 0);
-
-    final int result = a.computeOptimisticSamplesToAudit(riskLimit10);
-    final int expected = optimistic(riskLimit10, 0.001, 1, 0, 0, 0,
-        Audit.GAMMA);
-
-    assertEquals(result, expected);
-    assertEquals(a.optimisticSamplesToAudit.intValue(), expected);
-  }
-
-  /**
-   * Check that the correct optimistic sample size is computed for an NEB assertion
-   * with a small diluted margin and one 2-vote understatement.
-   */
-  @Test
-  public void testNEBOptimisticSampleOneTwoVoteUnderSmallMargin(){
-    Assertion a = createNEBAssertion("W", "L", "Test Contest",
-        10, 0.001, 100, Map.of(1L, -2), 0,
-        0, 0, 1, 0);
-
-    final int result = a.computeOptimisticSamplesToAudit(riskLimit10);
-    final int expected = optimistic(riskLimit10, 0.001, 0, 0, 0, 1,
-        Audit.GAMMA);
-
-    assertEquals(result, expected);
-    assertEquals(a.optimisticSamplesToAudit.intValue(), expected);
-  }
-
-  /**
-   * Check that the correct optimistic sample size is computed for an NEB assertion
-   * with a small diluted margin and one 2-vote overstatement.
-   */
-  @Test
-  public void testNEBOptimisticSampleOneTwoVoteOverSmallMargin(){
-    Assertion a = createNEBAssertion("W", "L", "Test Contest",
-        10, 0.001, 100, Map.of(1L, 2), 0,
-        0, 1, 0, 0);
-
-    final int result = a.computeOptimisticSamplesToAudit(riskLimit10);
-    final int expected = optimistic(riskLimit10, 0.001, 0, 1, 0, 0,
-        Audit.GAMMA);
-
-    assertEquals(result, expected);
-    assertEquals(a.optimisticSamplesToAudit.intValue(), expected);
-  }
-
-  @Test
-  public void testNEBOptimisticSampleOneOtherSmallMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleTwoOneVoteUnderSmallMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleTwoOneVoteOverSmallMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleTwoTwoVoteUnderSmallMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleTwoTwoVoteOverSmallMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleTwoOtherSmallMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleOneOneVoteOverOneTwoVoteOverSmallMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleOneOneVoteOverOneOneVoteUnderSmallMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleOneOneVoteOverOneTwoVoteUnderSmallMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleOneOneVoteOverOtherSmallMargin(){
-
-  }
-
-
-  @Test
-  public void testNEBOptimisticSampleOneTwoVoteOverOneOneVoteUnderSmallMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleOneTwoVoteOverOneTwoVoteUnderSmallMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleOneTwoVoteOverOtherSmallMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleOneOfEachTypeNoOverstatementsSmallMargin(){
-
-  }
-
-  // Dimension: large diluted margin
-  @Test
-  public void testNEBOptimisticSampleNoDiscrepanciesLargeMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleOneOneVoteUnderLargeMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleOneOneVoteOverLargeMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleOneTwoVoteUnderLargeMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleOneTwoVoteOverLargeMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleOneOtherLargeMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleTwoOneVoteUnderLargeMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleTwoOneVoteOverLargeMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleTwoTwoVoteUnderLargeMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleTwoTwoVoteOverLargeMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleTwoOtherLargeMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleOneOneVoteOverOneTwoVoteOverLargeMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleOneOneVoteOverOneOneVoteUnderLargeMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleOneOneVoteOverOneTwoVoteUnderLargeMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleOneOneVoteOverOtherLargeMargin(){
-
-  }
-
-
-  @Test
-  public void testNEBOptimisticSampleOneTwoVoteOverOneOneVoteUnderLargeMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleOneTwoVoteOverOneTwoVoteUnderLargeMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleOneTwoVoteOverOtherLargeMargin(){
-
-  }
-
-  @Test
-  public void testNEBOptimisticSampleOneOfEachTypeNoOverstatementsLargeMargin(){
-
-  }
 }
