2a2932786cf47dd3771eddce894c7ff710d1d5cf
vteague <vanessafromgit@thinkingcybersecurity.com>
2024-06-13 21:07:50 +1000

    ComparisonAuditController tests.

---
+++ server/eclipse-project/src/test/java/au/org/democracydevelopers/corla/controller/ComparisonAuditControllerTests.java
@@ -0,0 +1,177 @@
+/*
+Democracy Developers IRV extensions to colorado-rla.
+
+@copyright 2024 Colorado Department of State
+
+These IRV extensions are designed to connect to a running instance of the raire 
+service (https://github.com/DemocracyDevelopers/raire-service), in order to 
+generate assertions that can be audited using colorado-rla.
+
+The colorado-rla IRV extensions are free software: you can redistribute it and/or modify it under the terms
+of the GNU Affero General Public License as published by the Free Software Foundation, either
+version 3 of the License, or (at your option) any later version.
+
+The colorado-rla IRV extensions are distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along with
+raire-service. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+package au.org.democracydevelopers.corla.controller;
+
+import au.org.democracydevelopers.corla.model.ContestType;
+import au.org.democracydevelopers.corla.model.IRVComparisonAudit;
+import au.org.democracydevelopers.corla.model.vote.IRVParsingException;
+import au.org.democracydevelopers.corla.testUtils;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import us.freeandfair.corla.controller.ComparisonAuditController;
+import us.freeandfair.corla.controller.ContestCounter;
+import us.freeandfair.corla.csv.DominionCVRExportParser;
+import us.freeandfair.corla.model.*;
+import us.freeandfair.corla.persistence.Persistence;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
+import java.util.Properties;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static au.org.democracydevelopers.corla.testUtils.*;
+import static org.testng.Assert.*;
+import static us.freeandfair.corla.query.CastVoteRecordQueries.getMatching;
+import static us.freeandfair.corla.query.ContestQueries.forCounties;
+import static us.freeandfair.corla.query.CountyQueries.fromString;
+
+/**
+ * Test those aspects of the ComparisonAuditController that were altered for IRV:
+ * - an all-IRV contest is made into an IRVComparisonAudit,
+ * - an all-plurality contest is made into a (plain, plurality) ComparisonAudit,
+ * - a mixed-type contest, or a contest that is neither plurality nor IRV, throws an error.
+ */
+public class ComparisonAuditControllerTests {
+
+  /**
+   * Class-wide logger
+   */
+  private static final Logger LOGGER = LogManager.getLogger(ComparisonAuditControllerTests.class);
+
+  /**
+   * Container for the mock-up database.
+   */
+  static PostgreSQLContainer<?> postgres
+      = new PostgreSQLContainer<>("postgres:15-alpine")
+      // None of these actually have to be the same as the real database (except its name), but this
+      // makes it easy to match the setup scripts.
+      .withDatabaseName("corla")
+      .withUsername("corlaadmin")
+      .withPassword("corlasecret")
+      // .withInitScript("SQL/corlaInit.sql");
+      .withInitScript("SQL/corla-three-candidates-ten-votes-inconsistent-types.sql");
+
+  /**
+   * Blank properties for submitting to the DominionCVRExportParser instance.
+   */
+  private static final Properties blank = new Properties();
+
+  /**
+   * IRV contest name.
+   */
+  private static final String tinyIRV = "TinyExample1";
+
+  /**
+   * Plurality contest name.
+   */
+  private static final String tinyPlurality = "PluralityExample1";
+
+  /**
+   * Mixed IRV/plurality contest name - should produce errors.
+   */
+  private static final String tinyMixed = "PluralityExample2";
+
+  @BeforeClass
+  public static void beforeAll() {
+    postgres.start();
+    Properties hibernateProperties = new Properties();
+    hibernateProperties.setProperty("hibernate.driver", "org.postgresql.Driver");
+    hibernateProperties.setProperty("hibernate.url", postgres.getJdbcUrl());
+    hibernateProperties.setProperty("hibernate.user", postgres.getUsername());
+    hibernateProperties.setProperty("hibernate.pass", postgres.getPassword());
+    hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL9Dialect");
+    Persistence.setProperties(hibernateProperties);
+    Persistence.beginTransaction();
+
+  }
+
+  @AfterClass
+  public static void afterAll() {
+    postgres.stop();
+  }
+
+  /**
+   * The consistent IRV contest, tinyIRVExample1, should make an IRVComparisonAudit.
+   */
+  @Test
+  public void IRVContestMakesIRVAudit() {
+    // Set up the contest results from the stored data.
+    List<ContestResult> results = ContestCounter.countAllContests();
+
+    // Find all the contestResults for TinyIRV - there should be one.
+    List<ContestResult> tinyIRVResults = results.stream().filter(
+            cr -> cr.getContests().stream().anyMatch(co -> co.name().equals(tinyIRV))).toList();
+    assertEquals(1, tinyIRVResults.size());
+
+    ContestResult tinyIRV = tinyIRVResults.get(0);
+    ComparisonAudit irvAudit = ComparisonAuditController.createAuditOfCorrectType(tinyIRV, BigDecimal.valueOf(0.03));
+    assertTrue(irvAudit instanceof IRVComparisonAudit);
+  }
+
+  /**
+   * The consistent plurality contest, PluralityExample1, should make an IRVComparisonAudit.
+   */
+  @Test
+  public void pluralityContestMakesPluralityAudit() {
+    // Set up the contest results from the stored data.
+    List<ContestResult> results = ContestCounter.countAllContests();
+
+    // Find all the contestResults for TinyIRV - there should be one.
+    List<ContestResult> tinyPluralityResults = results.stream().filter(
+            cr -> cr.getContests().stream().anyMatch(co -> co.name().equals(tinyPlurality))).toList();
+    assertEquals(1, tinyPluralityResults.size());
+
+    ContestResult tinyPlurality = tinyPluralityResults.get(0);
+    ComparisonAudit audit = ComparisonAuditController.createAuditOfCorrectType(tinyPlurality, BigDecimal.valueOf(0.03));
+    // Should _not_ be an IRVComparisonAudit.
+    assertFalse(audit instanceof IRVComparisonAudit);
+  }
+
+  /**
+   * A contest that mixes plurality and IRV throws an exception when we try to create an audit.
+   */
+  @Test(expectedExceptions = RuntimeException.class)
+  public void inconsistentContestThrowsException() {
+    // Set up the contest results from the stored data.
+    List<ContestResult> results = ContestCounter.countAllContests();
+
+    // Find all the contestResults for TinyIRV - there should be one.
+    List<ContestResult> tinyMixedResults = results.stream().filter(
+            cr -> cr.getContests().stream().anyMatch(co -> co.name().equals(tinyMixed))).toList();
+    assertEquals(1, tinyMixedResults.size());
+
+    ContestResult tinyPlurality = tinyMixedResults.get(0);
+    ComparisonAudit audit = ComparisonAuditController.createAuditOfCorrectType(tinyPlurality, BigDecimal.valueOf(0.03));
+
+  }
+}
