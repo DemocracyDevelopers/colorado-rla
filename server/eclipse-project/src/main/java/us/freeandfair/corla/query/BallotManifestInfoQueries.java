7422e9ac302e1dade71bd48c69fc743c11140933
Matt Bernhard <mdb92nc@gmail.com>
2024-06-19 22:32:40 -0400

    removing unneeded import

--- server/eclipse-project/src/main/java/us/freeandfair/corla/query/BallotManifestInfoQueries.java
+++ server/eclipse-project/src/main/java/us/freeandfair/corla/query/BallotManifestInfoQueries.java
@@ -28,7 +28,6 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-import javax.swing.text.html.Option;
 
 import org.hibernate.Session;
 import org.hibernate.query.Query;
