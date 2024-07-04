c9a08fa96aaea4edb9c083d3d2aecd7ef3c2f773
vteague <vanessafromgit@thinkingcybersecurity.com>
2024-07-04 21:16:24 +1000

    Update name of get-assertions endpoint.

--- client/src/action/dos/exportAssertions.ts
+++ client/src/action/dos/exportAssertions.ts
@@ -1,7 +1,7 @@
 import { endpoint } from 'corla/config';
 
 export default () => {
-    const url = `${endpoint('export-assertions')}`;
+    const url = `${endpoint('get-assertions')}`;
 
     window.location.replace(url);
 };
