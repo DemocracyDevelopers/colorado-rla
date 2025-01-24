90bdc8d62855c046bc5d600cd38a2ea4de602147
Matt Bernhard <umbernhard@gmail.com>
2025-01-23 20:33:38 -0500

    Starting AuditInvestigationReportInfoTest

---
+++ server/eclipse-project/src/test/java/us/freeandfair/corla/model/AuditInvetigationReportInfoTest.java
@@ -0,0 +1,6 @@
+package us.freeandfair.corla.model;
+
+import au.org.democracydevelopers.corla.util.TestClassWithDatabase;
+
+public class AuditInvetigationReportInfoTest extends TestClassWithDatabase {
+}
