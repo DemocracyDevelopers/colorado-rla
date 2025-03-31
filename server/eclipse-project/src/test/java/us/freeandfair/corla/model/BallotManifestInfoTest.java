40552eb5318b4491f3cb70421b6f721bd107eaf0
Matt Bernhard <umbernhard@gmail.com>
2025-03-31 16:17:38 -0400

    BallotManifestInfo has full coverage

--- server/eclipse-project/src/test/java/us/freeandfair/corla/model/BallotManifestInfoTest.java
+++ server/eclipse-project/src/test/java/us/freeandfair/corla/model/BallotManifestInfoTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 import java.util.Optional;
 
 import static org.testng.AssertJUnit.*;
+import static us.freeandfair.corla.util.EqualsHashcodeHelper.nullableHashCode;
 
 public class BallotManifestInfoTest extends TestClassWithDatabase {
 
@@ -21,6 +22,10 @@ public class BallotManifestInfoTest extends TestClassWithDatabase {
     private static final Long start = 23L;
     private static final String uri = "bmi:1:1-Batch 1";
 
+    private static final String toString = "BallotManifestInfo [" + ", county_id=" + the_county_id +
+            ", scanner_id=" + the_scanner_id + ", batch_size=" +
+    the_batch_size + ", storage_container=" + the_storage_location + "]";
+
     private static final BallotManifestInfo bmi = new BallotManifestInfo(the_county_id,
             the_scanner_id,
             the_batch_id,
@@ -29,6 +34,23 @@ public class BallotManifestInfoTest extends TestClassWithDatabase {
             the_sequence_start,
             the_sequence_end);
 
+    private static final BallotManifestInfo other = new BallotManifestInfo(the_county_id,
+            the_scanner_id,
+            the_batch_id,
+            the_batch_size + 1,
+            the_storage_location,
+            the_sequence_start + 1,
+            the_sequence_end + 1);
+
+    private static final BallotManifestInfo otherCounty = new BallotManifestInfo(the_county_id + 1,
+           the_scanner_id,
+           the_batch_id,
+           the_batch_size + 1,
+           the_storage_location,
+           the_sequence_start + 1,
+           the_sequence_end + 1);
+
+    private static final BallotManifestInfo.Sort bmiSort = new BallotManifestInfo.Sort();
 
 
     @Test
@@ -49,10 +71,13 @@ public class BallotManifestInfoTest extends TestClassWithDatabase {
         assertEquals(uri, bmi.getUri());
 
         bmi.setUltimate(start);
-        assertEquals(start, bmi.sequenceStart());
+        assertEquals(start, bmi.ultimateSequenceStart);
         // For some reason I had to cast here?
         assertEquals((Long)(the_sequence_end - the_sequence_start), bmi.rangeSize());
         assertEquals((Long)(bmi.rangeSize() + start), bmi.ultimateSequenceEnd);
+
+        assertEquals(bmi.toString(), toString);
+        assertEquals(bmi.hashCode(), nullableHashCode(the_storage_location));
     }
 
     @Test
@@ -69,11 +94,25 @@ public class BallotManifestInfoTest extends TestClassWithDatabase {
         assertTrue(bmi.isHolding(bmi.ultimateSequenceEnd));
         assertTrue(bmi.isHolding(25L));
 
-        assertEquals(bmi.ballotPosition(rand.intValue()), (Integer)12323);
+        assertEquals(bmi.ballotPosition(rand.intValue()), rand);
         // Why are these types so inconsistent?
-        //assertEquals(bmi.imprintedID(new Long((long)rand), "1-Batch 1-12323");
+        assertEquals(bmi.imprintedID(new Long((long)rand)), "1-Batch 1-12345");
+    }
 
+    @Test
+    public static void testBMIEquality() {
+        assertFalse(bmi.equals("other"));
+        assertTrue(bmi.equals(bmi));
+        assertFalse(bmi.equals(other));
+    }
 
+    @Test
+    public static void testBMISort() {
+        assertEquals(bmiSort.compare(bmi, bmi), 0);
+        assertTrue(bmiSort.compare(bmi, other) < 0);
+        assertTrue(bmiSort.compare(other, bmi) > 0);
+        assertTrue(bmiSort.compare(bmi, otherCounty) < 0);
+        assertTrue(bmiSort.compare(otherCounty, bmi) > 0);
 
 
 
