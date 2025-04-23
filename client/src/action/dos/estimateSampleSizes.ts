eeaf8ffa1e38701e286b798e52f3992ddafeb4f7
charliecarlton <charliecarlton@gmail.com>
2025-04-23 08:23:22 -0600

    Download sample size csv with javascript to show spinner and success/failure statuses

--- client/src/action/dos/estimateSampleSizes.ts
+++
@@ -1,7 +0,0 @@
-import { endpoint } from 'corla/config';
-
-export default () => {
-    const url = `${endpoint('estimate-sample-sizes')}`;
-
-    window.location.replace(url);
-};
--- client/src/component/DOS/DefineAudit/EstimateSampleSizesPage.tsx
+++ client/src/component/DOS/DefineAudit/EstimateSampleSizesPage.tsx
@@ -1,8 +1,7 @@
+import {endpoint} from 'corla/config';
 import * as React from 'react';
 
-import { Breadcrumb, Button, Card, Intent } from '@blueprintjs/core';
-
-import estimateSampleSizes from 'corla/action/dos/estimateSampleSizes';
+import {Breadcrumb, Button, Card, Intent, Spinner} from '@blueprintjs/core';
 import DOSLayout from 'corla/component/DOSLayout';
 
 const Breadcrumbs = () => (
@@ -17,17 +16,27 @@ interface EstimateSampleSizesPageProps {
     forward: OnClick;
 }
 
+interface EstimateSampleSizesPageState {
+    generatingEstimatedSampleSizes: boolean;
+    generationFailed: boolean;
+    generationSucceeded: boolean;
+}
 
-class EstimateSampleSizesPage extends React.Component<EstimateSampleSizesPageProps> {
+class EstimateSampleSizesPage extends React.Component<EstimateSampleSizesPageProps, EstimateSampleSizesPageState> {
     public constructor(props: EstimateSampleSizesPageProps) {
         super(props);
+
+        this.state = {
+            generatingEstimatedSampleSizes: false,
+            generationFailed: false,
+            generationSucceeded: false,
+        };
     }
 
     public render() {
         const {
             forward,
         } = this.props;
-
 
         const main =
             <div>
@@ -38,8 +47,11 @@ class EstimateSampleSizesPage extends React.Component<EstimateSampleSizesPagePro
                     sample sizes for all contests in the database, which will be
                     automatically downloaded.
                 </p>
+                <div>
+                    {this.getSampleSizeGenerationStatus()}
+                </div>
                 <div className='control-buttons mt-default'>
-                    <Button onClick={estimateSampleSizes}
+                    <Button onClick={this.downloadEstimateSampleSizeSheet}
                             className='pt-button pt-intent-primary'>
                         Estimate
                     </Button>
@@ -56,6 +68,70 @@ class EstimateSampleSizesPage extends React.Component<EstimateSampleSizesPagePro
         return <DOSLayout main={ main } />;
     }
 
+    private getSampleSizeGenerationStatus() {
+        let statusHtml;
+
+        if (this.state.generatingEstimatedSampleSizes) {
+            statusHtml =
+                <Card className='mt-default'>
+                    <Spinner className='pt-medium' intent={Intent.PRIMARY}/>
+                    <div>Estimating Sample Sizes...</div>
+                </Card>;
+        } else if (this.state.generationSucceeded) {
+            statusHtml =
+                <div style={{color: 'green'}}>
+                    Estimated Sample Sizes CSV Downloaded
+                </div>;
+        } else if (this.state.generationFailed) {
+            statusHtml =
+                <div style={{color: 'red'}}>
+                    Error: Download failed. Please try again.
+                </div>;
+        }
+        return statusHtml;
+    }
+
+    private downloadEstimateSampleSizeSheet = async () => {
+        try {
+            this.setState({generatingEstimatedSampleSizes: true});
+            this.setState({generationFailed: false});
+            this.setState({generationSucceeded: false});
+            const response = await fetch(endpoint('estimate-sample-sizes'), { credentials: 'include'});
+            if (!response.ok || !response.body) {
+                this.setState({generationFailed: true});
+                return;
+            }
+
+            // Download sample size estimate csv file
+            const reader = response.body.getReader();
+            const chunks = [];
+
+            while (true) {
+                const { done, value } = await reader.read();
+                if (done) {
+                    break;
+                }
+                chunks.push(value);
+            }
+
+            const downloadBlob = new Blob(chunks);
+            const downloadUrl = URL.createObjectURL(downloadBlob);
+
+            // Dummy anchor element for download
+            const a = document.createElement('a');
+            a.href = downloadUrl;
+            a.download = 'sample_sizes.csv';
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+            URL.revokeObjectURL(downloadUrl);
+
+            this.setState({generatingEstimatedSampleSizes: false});
+            this.setState({generationSucceeded: true});
+        } catch (error) {
+            this.setState({generationFailed: true});
+        }
+    }
 }
 
 export default EstimateSampleSizesPage;
